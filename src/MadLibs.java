fe28e41d31fe200ddb793011ddbd59bba770688a
Kate Fulton <katesuzannefulton@gmail.com>
2015-02-24 16:09:18 -0800

    initialize an ArrayList

--- src/MadLibs.java
+++ src/MadLibs.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+
 /**
  * Created by katefulton on 2/24/15.
  */
 public class MadLibs {
+    ArrayList<String> libs = new ArrayList<String>();
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println("Call me ");
+        System.out.println("Some years ago - never mind how long precisely - having little or no ");
+        System.out.println("in my ");
+        System.out.println(", and nothing particular to interest me on ");
+        System.out.println("I thought I would ");
+        System.out.println("about a little");
     }
 }
