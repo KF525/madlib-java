e10fa84ef2d2a2a1c68f75c6c6fa08b50ca15722
Kate Fulton <katesuzannefulton@gmail.com>
2015-02-24 16:44:43 -0800

    refactor madlib and add inputs

--- src/MadLibs.java
+++ src/MadLibs.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Created by katefulton on 2/24/15.
@@ -7,18 +9,33 @@ public class MadLibs {
 
     public static void main(String[] args) {
         ArrayList<String> libs = new ArrayList<String>();
-        libs.add("Ishmael");
-        libs.add("money");
-        libs.add("purse");
-        libs.add("shore");
-        libs.add("sail");
-        libs.add("see");
-        System.out.print("Call me " + libs.get(0)); //noun
-        System.out.print(" Some years ago - never mind how long precisely - having little or no " + libs.get(1)); //noun
-        System.out.print(" in my " + libs.get(2)); //noun
-        System.out.print(" and nothing particular to interest me on " + libs.get(3)); //noun
-        System.out.print(" I thought I would " + libs.get(4)); //verb
-        System.out.print(" about a little and " + libs.get(5)); //verb
-        System.out.print(" the watery part of the world.");
+        ArrayList<String> mads = new ArrayList<String>();
+        String[] strings = {
+                "Call me ",
+                " Some years ago - never mind how long precisely - having little or no ",
+                " in my ",
+                " and nothing particular to interest me on ",
+                " I thought I would ",
+                " about a little and ",
+                " the watery part of the world."};
+        mads.addAll(Arrays.asList(strings)); //casts array object as a list
+
+        String s;
+        Scanner in = new Scanner(System.in);
+        String[] inputs = {"proper noun", "noun", "noun", "noun", "verb", "verb" };
+
+        for (int i = 0; i < inputs.length; i++ ) {
+            System.out.println("Choose a " + inputs[i]);
+            s = in.nextLine();
+            libs.add(s);
+        }
+
+        System.out.print( mads.get(0) + libs.get(0)); //noun
+        System.out.print( mads.get(1) + libs.get(1)); //noun
+        System.out.print( mads.get(2) + libs.get(2)); //noun
+        System.out.print( mads.get(3) + libs.get(3)); //noun
+        System.out.print( mads.get(4) + libs.get(4)); //verb
+        System.out.print( mads.get(5) + libs.get(5)); //verb
+        System.out.print( mads.get(6));
     }
 }
